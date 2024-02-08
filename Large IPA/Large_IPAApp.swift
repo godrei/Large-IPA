6aee50a672a675f663d400e6ff5f86803afb440e
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2024-02-08 15:52:02 +0100

    Initial Commit

---
+++ Large IPA/Large_IPAApp.swift
@@ -0,0 +1,17 @@
+//
+//  Large_IPAApp.swift
+//  Large IPA
+//
+//  Created by Krisztián Gödrei on 08/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Large_IPAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
